56bdba3b5135f7e5a7d727c01da41299a321fcb5
nknuecht <nknuecht@umich.edu>
2020-12-02 10:35:39 -0800

    dataloader edit

--- datasets.py
+++ datasets.py
@@ -195,7 +195,6 @@ class GeneralDataset(Dataset):
         if self.seg_transform: sample['seg_image'] = self.seg_transform(sample['seg_image'])
         if self.seg_transform: sample['seg_probs'] = self.seg_transform(sample['seg_probs'])
 
-        print('label', label)
         if tciaID != 0 and (label == 0 or label == 1): # scan from the tcia.
             ## if the sample is from the TCIA that means that you have survival (for sure)
             if gt_seg == 1:
